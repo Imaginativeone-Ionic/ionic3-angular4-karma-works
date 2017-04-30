9afdc6ac07a12f11e4206917b3e0f6d5e089df3f
Doug Franklin <doug@dougfranklin.com>
2017-04-30 06:57:20 -0400

    Tiny Advances with Tests

--- src/providers/magic-ball.spec.ts
+++ src/providers/magic-ball.spec.ts
@@ -5,6 +5,8 @@ describe('Magic 8 Ball Service', ()=>{
     it('should do nothing', ()=>{
 
         expect(true).toBeTruthy();
+        expect(1 + 1).toBe(2);
+        expect(2 + 2).toBe(4); // 5, this will fail
 
     });
 
